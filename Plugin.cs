ec9f0d0b7cfd7d540f209c2a6d660f4b0c7fa983
Trap <trapcodien@gmail.com>
2025-01-17 14:40:04 +0100

    chore: bump version to 2.0.0

--- Plugin.cs
+++ Plugin.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 
 namespace InteractableExfilsAPI
 {
-    [BepInPlugin("Jehree.InteractableExfilsAPI", "InteractableExfilsAPI", "1.5.1")]
+    [BepInPlugin("Jehree.InteractableExfilsAPI", "InteractableExfilsAPI", "2.0.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static ManualLogSource LogSource;
